231b4c849731c4d0b67eb1cd010639929a04ef15
DavitaR <92464615+DavitaR@users.noreply.github.com>
2022-11-22 07:58:57 -0500

    company class

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,11 @@
 import { User } from "./User";
+import { Company } from "./Company";
 
 const user = new User();
+const company = new Company();
 
-console.log(`my user is: ${JSON.stringify(user)}`);
+console.log(user);
+console.log(company);
+
+//google maps api key
+//AIzaSyBNLrJhOMz6idD05pzfn5lhA-TAw-mAZCU
